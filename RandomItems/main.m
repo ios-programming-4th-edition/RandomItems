2fbcec0863b90ca6b847e6e82c94c88e6a063643
Richard Millet <remillet@yahoo.com>
2014-03-01 21:57:27 -0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Richard E Millet on 3/1/14.
+//  Copyright (c) 2014 remillet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
